580d86388ea80b56a52746eb8c9f76fa5305b208
wesley-lewis <lewis.wes.23@gmail.com>
2023-07-04 21:43:39 +0530

    Bidirectional streaming done

---
+++ client/bi_stream.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/wesley-lewis/grpc-golang/proto"
+)
+
+func callHelloBidirectionalStreaming(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Printf("Bidirectional streaming has started")
+	stream, err := client.SayHelloBidirectionalStreaming(context.Background())
+	if err != nil {
+		log.Fatalf("could not send names: %v", err)
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for {
+			message, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("error while streaming: %v", err)
+			}
+
+			log.Println(message)
+		}
+		close(waitc)
+	}()
+
+	for _, name := range names.Names {
+		req := &pb.HelloRequest{
+			Name: name,
+		}
+
+		if err := stream.Send(req); err != nil {
+			log.Fatalf("error while sending the request: %v", err)
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+	stream.CloseSend()
+	<-waitc
+	log.Printf("Bidirectional streaming finished")
+}
--- client/main.go
+++ client/main.go
@@ -28,5 +28,6 @@ func main() {
 
 	// callSayHello(client)
 	// callSayHelloServerStream(client, names)
-	callSayHelloClientStream(client, names)
+	// callSayHelloClientStream(client, names)
+	callHelloBidirectionalStreaming(client, names)
 }
