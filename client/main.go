1ee862fe5e02cd59f78955228a25a5c6a7299d71
wesley-lewis <lewis.wes.23@gmail.com>
2023-07-04 20:40:21 +0530

    basic client-server communication

---
+++ client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/wesley-lewis/grpc-golang/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := pb.NamesList{
+	// 	Names: []string{"Wesley", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+}
